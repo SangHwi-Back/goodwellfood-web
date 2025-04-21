7d0f0743c7e95db6d182147b837428b667474b01
sanghwiback <sanghwiback@gmail.com>
2025-04-21 11:24:28 +0900

    Add placeholder image for ProductCard component
    Introduced a default placeholder image to handle cases where product images are unavailable or fail to load. Updated the ProductCard component to dynamically set the placeholder on image errors and added the SVG asset to the public directory.

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react';
 import {ProductCardSize, getProductCardTailwindSize} from "@/components/ui/ProductCardSize";
 import ProductCardTitle from "@/components/ui/ProductCardTitle";
@@ -22,12 +24,25 @@ type Props = {
 export default function ProductCard({ product }: Props) {
   const {name, price, image, size} = product;
   const style = `flex overflow-hidden ${getProductCardTailwindSize(size ?? ProductCardSize.Default)} flex-shrink-0 flex-col shadow-md bg-white rounded-md`;
+  const _image = image.length == 0 ? "/placeholder.svg" : image;
+  const [imgSrc, setImgSrc] = React.useState(_image);
+  const handleImageError = () => {
+    setImgSrc("/placeholder.svg"); // 이미지 로드 실패 시 대체 이미지 경로 설정
+  };
+
 
   return (
     <div className={style}>
       {/* 이미지 섹션 */}
       <div className="h-7/10 w-full object-cover">
-        <Image src={image} alt={name} className={"w-full h-full border-gray-200 border-1 rounded-t-md"}/>
+        <Image
+          src={imgSrc}
+          alt={name}
+          className={"w-full h-full border-gray-200 border-1 rounded-t-md"}
+          width={1}
+          height={1}
+          onError={handleImageError}
+        />
       </div>
       {/* 정보 섹션 */}
       <div className="border-b border-x border-gray-200">
